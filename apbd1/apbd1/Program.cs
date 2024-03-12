7057ce9347d33684f7ca25c1a2e16d17ffb1eb87
s28781 <s28781@pjwstk.edu.pl>
2024-03-12 13:30:48 +0100

    add avg method

--- apbd1/apbd1/Program.cs
+++ apbd1/apbd1/Program.cs
@@ -1,3 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine("hehehe");
+}
+
+
+static double getAvg(int[] nums)
+{
+    int sum = 0;
+    foreach (var num in nums)
+    {
+        sum += num;
+    }
+
+    return (double) sum / nums.Length;
+}
